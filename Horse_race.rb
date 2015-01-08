fd5b80aa38e21a8583bf7869bcdc58211e90b3fb
Kimokeo Rabang <kimokerava@gmail.com>
2015-01-08 13:25:55 -0500

    forgot to save

---
+++ Horse_race.rb
@@ -0,0 +1,14 @@
+# Class for "your horse"
+# Class for other horses
+# define horses
+# create movement operation (rand)
+# create win condition
+# cheat code
+
+class.Kimosabe
+
+end
+
+class.Other_horses
+
+end
