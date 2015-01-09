8b9b99a119bf9193d2fa98be4ca662171500f165
Kimokeo Rabang <kimokerava@gmail.com>
2015-01-09 11:54:31 -0500

    Learning more efficient ways to do this

--- Horse_race.rb
+++ Horse_race.rb
@@ -1,56 +1,67 @@
-# Class for "your horse"
-# Class for other horses
+# Class for Horses
 # define horses
 # create movement operation (rand)
 # create win condition
 # cheat code
+class Track
+	attr_accessor :length
+	attr_accessor :fence
 
-class Donkey
+	def initialize
+		length = 100
+		fence = "#"
+	end
+
+	def horse_track
+		puts (:length).times.(:fence)
+	end
+end
+
+class Horse
 	attr_accessor :name
 	attr_accessor :speed
 	attr_accessor :image
 	attr_accessor :steps
 
+	def run 
+	if Horse {[1,2,3]} steps.to_i > 100
+		puts "you lose"
+		break
+	else
+		# if :steps <100
+		puts "Cheer on your horse"
+		cheer = gets.chomp
+
+		# else
+ 		# break
+ 		# end
+ 	end
 end
 
-class Other_horses
-	attr_accessor :name
-	attr_accessor :speed
-	attr_accessor :image
-	attr_accessor :steps
 
-end
 
-distance = (:speed += :steps)
 
-horse1 = Other_horses.new
+horse1 = Horse.new
 horse1.name = "stalin"
 horse1.speed = "rand(0..7)"
 horse1.image = ":{"
 horse1.steps = 0
 
-horse2 = Other_horses.new
+horse2 = Horse.new
 horse2.name = "hussain"
 horse2.speed = "rand(2..5)"
 horse2.image = "=E"
 horse2.steps = 0
 
-horse3 = Other_horses.new
+horse3 = Horse.new
 horse3.name = "dear_leader_kim_jong_un"
 horse3.speed = "rand(3..4)"
 horse3.image = ":("
 horse3.steps = 0
 
-horse4 = Donkey.new
+horse4 = Horse.new
 horse4.name = "kimosabe"
 horse4.speed = "rand(1..5)"
 horse4.image = "XD"
 horse4.steps = 0
 
-loop do
-	if :steps < 50
-	puts [distance].times print "=" Other_horses.image
-	puts [distance].times print "=" Donkey.image
-	else
-	end
-end
